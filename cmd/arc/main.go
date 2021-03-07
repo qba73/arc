1751f40b387e087038fa3e0a80d268ce2b57ee59
Jakub Jarosz <jakub@jarosz.dev>
2021-03-07 11:21:41 +0200

    add cli for the tool

--- cmd/arc/main.go
+++ cmd/arc/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/qba73/arct/internal/arc"
+)
 
 func main() {
-	fmt.Println("Processing...")
+	var filein, fileout string
+	flag.StringVar(&filein, "input", "", "ArcTool log file to process")
+	flag.StringVar(&fileout, "output", "", "Output CSV file")
+	flag.Parse()
+	verifyFlags(filein, fileout)
+
+	if err := run(filein, fileout); err != nil {
+		fmt.Println("Encountered an error, exiting")
+		os.Exit(1)
+	}
+}
+
+func run(fin, fout string) error {
+	if err := arc.GenerateReport(fin, fout); err != nil {
+		return err
+	}
+	return nil
+}
+
+func verifyFlags(in, out string) {
+	if in == "" || out == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
 }
